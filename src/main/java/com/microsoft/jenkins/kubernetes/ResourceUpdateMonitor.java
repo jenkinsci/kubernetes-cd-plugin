5d72ca941124083eef6f4dbcafc8c03faaf59b3d
Daniel Becker <db@fino.digital>
2018-09-03 09:29:02 +0200

    Updated kubernetes-client lib to newest version

--- src/main/java/com/microsoft/jenkins/kubernetes/ResourceUpdateMonitor.java
+++ src/main/java/com/microsoft/jenkins/kubernetes/ResourceUpdateMonitor.java
@@ -7,16 +7,16 @@
 package com.microsoft.jenkins.kubernetes;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
-import io.fabric8.kubernetes.api.model.Job;
+import io.fabric8.kubernetes.api.model.batch.Job;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.ReplicationController;
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.extensions.DaemonSet;
-import io.fabric8.kubernetes.api.model.extensions.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DaemonSet;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.api.model.extensions.Ingress;
-import io.fabric8.kubernetes.api.model.extensions.ReplicaSet;
+import io.fabric8.kubernetes.api.model.apps.ReplicaSet;
 
 public interface ResourceUpdateMonitor {
     void onSecretUpdate(Secret original, Secret current);
